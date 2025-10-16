d13f187e83f7e9e3c2158ff6033e153c47533e98
ramune314159265 <ramune314159265@gmail.com>
2025-10-17 01:50:04 +0900

    自動保存

--- src/atoms/pypen.js
+++ src/atoms/pypen.js
@@ -1,9 +1,20 @@
 import { atom, useAtom } from 'jotai'
+import { useEffect } from 'react'
+import { debounce } from '../utils/debounce'
 
-export const pyPenAtom = atom('')
+export const pyPenAtom = atom(localStorage.getItem('ramune314159265.pypeneditor.pypencontent') === null ?
+	'' :
+	localStorage.getItem('ramune314159265.pypeneditor.pypencontent')
+)
 
 export const usePyPen = () => {
 	const [pyPenContent, setPyPenContent] = useAtom(pyPenAtom)
+
+	useEffect(() => {
+		debounce(() => {
+			localStorage.setItem('ramune314159265.pypeneditor.pypencontent', pyPenContent)
+		}, 1000)()
+	}, [pyPenContent])
 
 	return [pyPenContent, { setPyPenContent }]
 }
--- src/atoms/python.js
+++ src/atoms/python.js
@@ -1,9 +1,20 @@
 import { atom, useAtom } from 'jotai'
+import { useEffect } from 'react'
+import { debounce } from '../utils/debounce'
 
-export const pythonAtom = atom('')
+export const pythonAtom = atom(localStorage.getItem('ramune314159265.pypeneditor.pythoncontent') === null ?
+	'' :
+	localStorage.getItem('ramune314159265.pypeneditor.pythoncontent')
+)
 
 export const usePython = () => {
 	const [pythonContent, setPythonContent] = useAtom(pythonAtom)
+
+	useEffect(() => {
+		debounce(() => {
+			localStorage.setItem('ramune314159265.pypeneditor.pythoncontent', pythonContent)
+		}, 1000)()
+	}, [pythonContent])
 
 	return [pythonContent, { setPythonContent }]
 }
