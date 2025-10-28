595f45e4a6c95b6793835d023803c12b50f0ff1a
ramune314159265 <ramune314159265@gmail.com>
2025-10-28 17:21:33 +0900

    拡張子登録

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,38 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["web-tree-sitter"]
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'PyPenエディター',
+        theme_color: '#fff',
+        display: 'standalone',
+        lang: 'ja-jp',
+        icons: [
+          {
+            src: 'icon.ico',
+            sizes: '144x144',
+            type: 'image/vnd.microsoft.icon'
+          }, {
+            src: 'icon.png',
+            sizes: '144x144',
+            type: 'image/png'
+          }
+        ],
+        file_handlers: [
+          {
+            action: "/",
+            accept: {
+              "text/pypen": [".PyPEN"],
+            }
+          }
+        ]
+      }
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
